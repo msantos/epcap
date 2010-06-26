00e85c7bc9ed623e8b1e59ae6d66fb7cbbdfc937
Michael Santos <michael.santos@gmail.com>
2010-06-26 14:08:28 -0400

    Adjust default values for records

--- include/epcap_net.hrl
+++ include/epcap_net.hrl
@@ -43,7 +43,7 @@
 -record(ipv4, {
         v = 4, hl = 5, tos = 0, len = 20,
         id = 0, df = 0, mf = 0,
-        off = 0, ttl = 0, p = ?IPPROTO_TCP, sum = 0,
+        off = 0, ttl = 64, p = ?IPPROTO_TCP, sum = 0,
         saddr = {127,0,0,1}, daddr = {127,0,0,1}
     }).
 
@@ -64,7 +64,7 @@
     }).
 
 -record(udp, {
-        sport = 0, dport = 0, ulen = 0, sum = 0
+        sport = 0, dport = 0, ulen = 8, sum = 0
     }).
 
 -record(icmp, {
