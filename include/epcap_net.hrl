64255ecca8e81fb3d58c9868a44e9c9bbdf96df5
Michael Santos <michael.santos@gmail.com>
2010-12-04 07:18:39 -0500

    Set the default TCP offset to 5

--- include/epcap_net.hrl
+++ include/epcap_net.hrl
@@ -86,7 +86,7 @@
         sport = 0, dport = 0,
         seqno = 0,
         ackno = 0,
-        off = 0, cwr = 0, ece = 0, urg = 0, ack = 0,
+        off = 5, cwr = 0, ece = 0, urg = 0, ack = 0,
         psh = 0, rst = 0, syn = 0, fin = 0, win = 0,
         sum = 0, urp = 0,
         opt = <<>>
