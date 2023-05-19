106e763520510afa7b7d8bdfeb0e402a46ea3496
Michael Santos <michael.santos@gmail.com>
2023-05-19 06:42:36 -0400

    rlimit: limit to max open fd
    libpcap may open additional file descriptors such as for polling by
    eventfd(2). If the rlimit process restrictions are used, find the largest
    open file descriptor by testing all fd's from the max open file descriptor
    limit (`ulimit -n`).
    
    With this restriction, an epcap process still has the capability to
    access files or the network by closing and reopening a fd. The
    capability to access files is limited by the call to chroot().
    
    Thanks @jephthai !
    
    Fixes #33

--- c_src/restrict_process_rlimit.c
+++ c_src/restrict_process_rlimit.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2020 Michael Santos <michael.santos@gmail.com>. All
+/* Copyright (c) 2017-2023 Michael Santos <michael.santos@gmail.com>. All
  * rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,26 +32,24 @@
 
 #include "epcap.h"
 
-/* On some platforms (Linux), poll() (used by pcap)
- *  * will return EINVAL if RLIMIT_NOFILES < numfd */
-#ifndef EPCAP_RLIMIT_NOFILES
-#define EPCAP_RLIMIT_NOFILES 0
-#warning "Using default value of EPCAP_RLIMIT_NOFILES=0"
-#endif
+static int fdlimit(int lowfd);
 
 int restrict_process_capture() {
   struct rlimit rl = {0};
+  int maxfd;
+
+  maxfd = fdlimit(STDERR_FILENO);
+
+  rl.rlim_cur = maxfd;
+  rl.rlim_max = maxfd;
+
+  if (setrlimit(RLIMIT_NOFILE, &rl) != 0)
+    return -1;
 
   if (setrlimit(RLIMIT_FSIZE, &rl) != 0)
     return -1;
 
   if (setrlimit(RLIMIT_NPROC, &rl) != 0)
-    return -1;
-
-  rl.rlim_cur = EPCAP_RLIMIT_NOFILES;
-  rl.rlim_max = EPCAP_RLIMIT_NOFILES;
-
-  if (setrlimit(RLIMIT_NOFILE, &rl) != 0)
     return -1;
 
   return 0;
@@ -70,5 +68,26 @@ int restrict_process_supervisor() {
     return -1;
 
   return 0;
+}
+
+/* A previous call to chroot(2) prevents using /dev/fd to find the
+ * max opened fd by the process. Fallback to testing each fd: with a high
+ * RLIMIT_NOFILE, this check will be slow.
+ */
+static int fdlimit(int lowfd) {
+  struct rlimit rl = {0};
+  int fd;
+
+  if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+    return -1;
+
+  for (fd = rl.rlim_cur; fd > lowfd; fd--) {
+    if (fcntl(fd, F_GETFD, 0) == -1)
+      continue;
+
+    return fd;
+  }
+
+  return lowfd;
 }
 #endif
